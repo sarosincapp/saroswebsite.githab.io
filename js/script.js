48cb421d1e74ea7ec0ebeae719f5662eab274da6
sarosincapp <dim.drvlg@gmail.com>
2020-10-18 20:43:19 +0400

    Добавил скриншоты на скрол изображений. обновил таз сайта.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows:false,
+    dots:true,
+    adaptiveHeight: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 1500
+  });
+});
